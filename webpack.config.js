fa457884ef4b06d1cdec2dda4fd0b733e2df7644
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2020-11-10 07:33:05 +0000

    chore: webpack config, package version update, dependencies config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,68 @@
 const path = require('path');
 
 module.exports = (env) => (
-    {
-        mode: env && env.production ? 'production' : 'development',
-        entry: path.resolve(__dirname, 'index.js'),
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'sketchmark.js',
-            library: 'SketchMark',
-            libraryTarget: 'umd',
-            libraryExport: 'default'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: [
-                            '@babel/plugin-transform-object-assign'
-                        ]
+    [
+        {
+            mode: env && env.production ? 'production' : 'development',
+            target: 'web',
+            entry: path.resolve(__dirname, './src/sketchmark.js'),
+            output: {
+                path: path.resolve(__dirname, 'dist'),
+                filename: 'sketchmark.js',
+                library: 'SketchMark',
+                libraryTarget: 'umd',
+                libraryExport: 'default'
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: [
+                                '@babel/plugin-transform-object-assign',
+                                '@babel/plugin-proposal-class-properties',
+                            ]
+                        }
                     }
-                }
-            ]
-        }
-    }
+                ]
+            },
+            devtool: env && env.production ?  undefined : 'source-map',
+            devServer: {
+                contentBase: path.join(__dirname, 'dist'),
+                compress: true,
+                port: 9000,
+                publicPath: '/dist'
+            }
+        },
+        {
+            mode: 'production',
+            target: 'webworker',
+            entry: path.resolve(__dirname, './src/sketchmark.worker.js'),
+            output: {
+                path: path.resolve(__dirname, 'dist'),
+                filename: 'sketchmark.worker.js',
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: [
+                                '@babel/plugin-transform-object-assign',
+                                '@babel/plugin-proposal-class-properties',
+                            ]
+                        }
+                    }
+                ]
+            },
+            devtool: env && env.production ?  undefined : 'source-map',
+        },
+    
+    ]
 )
