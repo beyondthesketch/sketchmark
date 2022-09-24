457d43175979bf37976b551795c51226f4ef263c
Nazeerudeen Ruhomutally <NajuDev@users.noreply.github.com>
2022-09-24 15:31:07 +0100

    feat: WIP updating states

--- src/sketchmark.js
+++ src/sketchmark.js
@@ -1,11 +1,21 @@
 import SKETCHMARK_REGISTRY from './constants/registry';
+import SKETCHMARK_ATTRIBUTE_PREFIX from './constants/sketchmark-attributes';
 import createModel from './functions/create-model';
 import fetchTemplate from './functions/fetch-templates';
 import updateModel from './functions/update-model';
 import { SketchmarkWorker } from './worker/worker';
 import renderUpdates from './functions/render-updates';
 
-function handleUpdates(msg) {
+function setAppUpdating(status) {
+    this.roots.forEach((el) => 
+        status
+            ? el.dataset.smUpdating = 'true'
+            : el.removeAttribute('data-sm-updating')
+    );
+}
+
+function handleUpdates(msg, app) {
+    const appUpdating = setAppUpdating.bind(app);
     const { data } = msg;
     const updates = data;
 
@@ -20,25 +30,45 @@ function handleUpdates(msg) {
     );
 
     if (updates && updates.length) {
+        // event approach
+        const updatingEvent = new CustomEvent(
+            'update',
+            {
+                detail: {
+                    updates
+                }
+            }
+        );
+        app.dispatchEvent(updatingEvent);
+
+        appUpdating(true);
+
         // **> preUpdate
+        console.log(this.preUpdate.constructor.name);
         this.preUpdate();
         // **> preRender
         this.preRender();
-        renderUpdates(updates, this.template);
+
+        renderUpdates(updates, this.root);
+
         // **> postRender
         this.postRender();
         // **> postUpdate;
         this.postUpdate();
+
+        appUpdating(false);
     }
 
     return updates;
 }
 
 // @TODO: Wrap this in function? so that attempts to reinit existing views does not return anything and does not register a var/const/let
-export default class Sketchmark {
-    #model;
+export default class Sketchmark extends EventTarget {
+    #root;
     #sketchmarkModel;
+
     constructor(source, config = {}) {
+        super();
         if (SKETCHMARK_REGISTRY[source]) {
             console && console.warn(`'${source}' view already initialised! To reinitialise this view, you must first quit the current view`);
             console && console.warn(`...this instance will be an alias for'${source}'`);
@@ -67,9 +97,13 @@ export default class Sketchmark {
         preInit();
         
         // ==> initialise
-        const model = fetchTemplate(source);
+        const model = fetchTemplate.call(this, source);
+        this.nodes = model;
+
         const sketchmarkModel = createModel(source, lifecycles, model[source], model.repeats);
-        this.#model = model;
+
+        this.#root = document.querySelectorAll(`[${SKETCHMARK_ATTRIBUTE_PREFIX}-source="${source}"]`);
+
         this.#sketchmarkModel = sketchmarkModel;
 
         // **> postInit
@@ -110,12 +144,14 @@ export default class Sketchmark {
             }
         }
 
-
         SketchmarkWorker.addEventListener(
             'message',
-            handleUpdates.bind(this.#sketchmarkModel)
+            (msg) => handleUpdates.bind(this.#sketchmarkModel)(msg, this)
         );
+    }
 
+    get roots() {
+        return this.#root;
     }
 
     get _model() {
